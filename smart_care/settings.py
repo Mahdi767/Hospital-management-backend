b879af3fc62eb3d7e0ee1778b70d8c1c177de796
Mahdi Hasan <mehedi49891@gmail.com>
2026-03-07 15:27:36 +0600

    Fix cors

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
      'django_filters',
      'drf_redesign',
+     "corsheaders",
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -68,6 +69,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,6 +83,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'smart_care.urls'
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500/",
+    
+]
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
